85ed9d898b76301509d8f05cb897b83171eb2983
Роман Маїк <maikroman@dc.arts>
2022-07-12 17:31:35 +0300

    style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,8 @@
 function header() {
     return (
         <header className='header'>
-            <img src='https://stmatthewsplumbing.com/wp-content/uploads/2020/01/starbucks-logo-png-transparent.png'></img>
-            <span className='logo-span'>Social Media</span>
+            <img src='http://ibthemespro.com/docs/beny/img/side-nav/cmm4.png'></img>
+            <span className='logo-span'>Social React World</span>
         </header>
     )
 }
